155f0c2e033dacc3765a45c8f22ebcfcf97ef247
Paul Borawski <paulborawski14@gmail.com>
2016-01-30 21:35:29 -0800

    I N I T I A L C O M M I T

---
+++ paulborawski.js
@@ -0,0 +1,12 @@
+var port = 30444;
+var app = require('express')();
+
+app.use(require('compression')());
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended:true }));
+app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));
+app.use(require('express').static('public'));
+
+require('http').createServer(app).listen(port,function(){
+  console.log('server is running you beeezy, on port', port)
+});
---
+++ readme.markdown
@@ -0,0 +1,6 @@
+# paulborawski.com
+
+my personal website
+
+# config
+im running under nginx proxy. see conf folder for specifics.
