b951bb95051ebccee41c70f7af9103fcf66628d7
Paul Borawski <paulborawski14@gmail.com>
2018-07-01 09:13:29 -0500

    testing a redirect with nginx

--- paulborawski.js
+++ paulborawski.js
@@ -4,7 +4,7 @@ var app = require('express')();
 app.use(require('compression')());
 app.use(require('body-parser').json());
 app.use(require('body-parser').urlencoded({ extended:true }));
-app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));
+//app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));
 app.use(require('express').static('public'));
 
 require('http').createServer(app).listen(port,function(){
